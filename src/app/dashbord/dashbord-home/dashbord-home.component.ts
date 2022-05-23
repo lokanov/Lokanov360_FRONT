8312c99d3e13abbd53e3d8b316192bb7667800bf
contact@lokanovpro.com <contact@lokanovpro.com>
2022-05-23 13:29:37 +0000

    commit

--- src/app/dashbord/dashbord-home/dashbord-home.component.ts
+++ src/app/dashbord/dashbord-home/dashbord-home.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { User } from 'src/app/_model/User';
+import { Video } from 'src/app/_model/Video';
+import { Visit } from 'src/app/_model/Visit';
+import { VideoService } from 'src/app/_service/video.service';
+import { VisitService } from 'src/app/_service/visit.service';
 
 @Component({
   selector: 'app-dashbord-home',
@@ -8,14 +14,85 @@ import { User } from 'src/app/_model/User';
 })
 export class DashbordHomeComponent implements OnInit {
 
+  visits: Visit[] = [];
+  visit: Visit =  new Visit();
+  visitNew: Visit = new Visit();
+  visitUpdate: Visit = new Visit();
+  videos: Video[] = [];
+  visitDelete: Visit = new Visit();
 
-  user = new User();
-  userConnected = new  User();
+  //modalRef: BsModalRef = new BsModalRef();
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor( private router: Router, private visitService : VisitService) { }
+
+
+  ngOnInit(): void {
+    this.loadReferences();
   }
 
+  config = {
+    backdrop: false,
+    animate: true,
+    ignoreBackdropClick: true,
+    class: 'modal-lg',
+  };
+    configC = {
+    backdrop: false,
+    animate: true,
+    ignoreBackdropClick: true,
+    class: 'modal-sm',
+  };
+
+  loadReferences()
+  {
+    this.visitService.getVisit().subscribe(
+      (value) => {
+      this.visits = value;
+      console.log(this.visits);
+    });
+}
+
+/*
+confirm(visit: Visit){
+  this.visitDelete = visit;
+}*/
+
+
+onDelete(visit:Visit){
+  //this.modalService.hide();
+  this.visitService.deleteVisit(visit).subscribe(
+     (value) => {
+       this.visit = value;
+       alert('Visite supprimée avec succés!!!');
+       this.ngOnInit();
+       console.log(this.visit);
+     }
+  );
 
 }
+
+/*
+formUpdate(visit: Visit,template: TemplateRef<any>) {
+  this.visitUpdate = visit;
+  this.modalRef = this.modalService.show(template, this.config);
+}
+
+onUpdate(visit: Visit) {
+  visit.setId(this.visitUpdate.id);
+  this.visitService.updateVisit(visit).subscribe(
+    () => {
+      this.ngOnInit();
+      alert('Visit modifiée avec succés!!!');
+        //this.router.navigate(['/list']);
+        console.log(this.visit);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}*/
+
+}
+
+ 
--- src/app/dashbord/dashbord-lokanov-home/dashbord-lokanov-home.component.ts
+++ src/app/dashbord/dashbord-lokanov-home/dashbord-lokanov-home.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { User } from 'src/app/_model/User';
+import { Video } from 'src/app/_model/Video';
+import { Visit } from 'src/app/_model/Visit';
+import { VideoService } from 'src/app/_service/video.service';
+import { VisitService } from 'src/app/_service/visit.service';
 
 @Component({
   selector: 'app-dashbord-lokanov-home',
@@ -7,9 +14,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashbordLokanovHomeComponent implements OnInit {
 
-  constructor() { }
+  user = new User();
+  userConnected = new  User();
+
+  videos:      Video[] = [];
+  video:       Video =  new Video();
+  videoNew:    Video = new Video();
+  videoUpdate: Video = new Video();
+  videoDelete: Video = new Video();
+  modalRef:    BsModalRef = new BsModalRef();
+
+  constructor( private router: Router, private videoService : VideoService,
+    private modalService : BsModalService) { }
 
   ngOnInit(): void {
+    this.loadReferences();
+    }
+
+  config = {
+    backdrop: false,
+    animate: true,
+    ignoreBackdropClick: true,
+    class: 'modal-lg',
+  };
+    configC = {
+    backdrop: false,
+    animate: true,
+    ignoreBackdropClick: true,
+    class: 'modal-sm',
+  };
+
+  loadReferences()
+  {
+    this.videoService.getVideo().subscribe(
+      (value) => {
+      this.videos = value;
+      console.log(this.videos);
+    });
   }
 
+
+onDelete(video:Video)
+{
+  //this.modalService.hide();
+  this.videoService.deleteVideo(video).subscribe(
+     (value) => {
+       this.video = value;
+       alert('Video supprimée avec succés!!!');
+       this.ngOnInit();
+       console.log(this.video);
+     }
+  );
 }
+
+formUpdate(video: Video,template: TemplateRef<any>) {
+  this.videoUpdate = video;
+  this.modalRef = this.modalService.show(template, this.config);
+}
+onUpdate(video: Video)
+ {
+  video.setId(this.videoUpdate.id);
+  this.videoService.updateVideo(video).subscribe(
+    () => {
+      this.ngOnInit();
+      alert('Video modifiée avec succés!!!');
+        //this.router.navigate(['/list']);
+        console.log(this.video);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}
+
+/*
+confirm(video: Video){
+  this.videoDelete = video;
+}
+decline(){
+  this.modalService.hide();
+}
+templateConfirms(video:Video,template: TemplateRef<any>) {
+  this.modalRef = this.modalService.show(template, this.configC);
+  this.confirm(video);
+}
+*/
+
+}
+
